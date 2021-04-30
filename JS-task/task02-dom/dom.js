544abdc929cee60d4d13c4268f09be22e54d398d
Luis <skorpio19691102@freemail.hu>
2021-04-30 10:44:12 +0200

    1feladat kész és a 2-dik félig

--- JS-task/task02-dom/dom.js
+++ JS-task/task02-dom/dom.js
@@ -1,4 +1,10 @@
 function changeOuterLinks() {}
-
+const links = document.querySelectorAll('nav#link-list a');
+    links.forEach( item => {
+        if (item.innerHTML.includes('outer-link')) {
+            item.setAttribute('target', '_blank');
+            item.innerHTML = `<strong>${item.innerHTML}</strong>`;
+        }
+    })
 
 export { changeOuterLinks };
